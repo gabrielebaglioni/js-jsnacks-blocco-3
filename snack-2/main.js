3e5bbacf5fae2f7b1b8fc1e07d4c816d3455753a
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 14:48:45 +0200

    inizializzo il mio programma

---
+++ snack-2/main.js
@@ -0,0 +1,6 @@
+//faccio inserire all'utente un numero
+const N = Number(prompt("inserisci un numero"));
+//verifico che sia un nuemero a tutti gli effetti
+while(isNaN(N)){
+   N = Number(prompt("INSERISCI UN NUMERO!!!!"))
+}
