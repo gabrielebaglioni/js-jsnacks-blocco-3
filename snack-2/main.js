faf6a17ce82b648a3c594da1fcd1a44b1d87e07d
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 16:12:49 +0200

    logica + primo if non funzionante implementato

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,19 +1,20 @@
 //faccio inserire all'utente un numero
-const N = Number(prompt("inserisci un numero"));
 //verifico che sia un nuemero a tutti gli effetti
-while(isNaN(N)){
-   N = Number(prompt("INSERISCI UN NUMERO!!!!"))
-}
-//creo un array vuoto per inserirci N array
-let nArray = [];
-//creo un ciclo che ha i.lengght=N come indice per stampare tanti array quanto è il valore di N
-for(let i = 0; i < N; i++){
-   // nArray.push([i]);
-   const nArray = []
-//agiungo nei miei N array 10 numeri casuli da 1 a 100 
-      for(let x = 0; x < 10; x++ ){
-         nArray.push(Math.floor(Math.random() * 100) + 1);  
+const N = Number(prompt("inserisci un numero"));
+      while(isNaN(N)){
+         N = Number(prompt("INSERISCI UN NUMERO!!!!"))
       }
-      console.log(nArray);
-}
+         //creo un array vuoto per inserirci N array
+         let nArray = [];
+         //creo un ciclo che ha i.lengght=N come indice per stampare tanti array quanto è il valore di N
+         for(let i = 0; i < N; i++){
+            // nArray.push([i]);
+            const nArray = []
+         //agiungo nei miei N array 10 numeri casuli da 1 a 100 
+               for(let x = 0; x < 10; x++ ){
+                  nArray.push(Math.floor(Math.random() * 100) + 1);  
+               }
+               console.log(nArray);
+         }
+
 
---
+++ snack-3/main.js
@@ -0,0 +1,12 @@
+//crea due elementi div html
+//i due div dovranno avere due classi diverse
+const container = document.querySelector(".container")
+const red = document.querySelector(".red")
+red.id = "red";
+const green = document.querySelector(".green")
+green.id = "green";
+//creo un array di numeri
+const numeri = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+for(let i = 0; i < numeri.length; i++){
+   if(numeri[i] % 2 === 0)
+}
