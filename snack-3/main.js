6e80bfd71c196e5efdf50ad689375059055d2296
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 16:43:03 +0200

    push finale

--- snack-3/main.js
+++ snack-3/main.js
@@ -8,5 +8,16 @@ green.id = "green";
 //creo un array di numeri
 const numeri = [1, 2, 3, 5, 6, 7, 8, 9, 10]
 for(let i = 0; i < numeri.length; i++){
-   if(numeri[i] % 2 === 0)
+   //pari green
+   if(numeri[i] % 2 === 0){
+      red.innerHTML += numeri[i];
+      red.style.color = "red";
+   }
+   else{
+      green.innerHTML += numeri[i];
+      green.style.color = "green";
+   }
+   
+
 }
+console.log(green, red)
