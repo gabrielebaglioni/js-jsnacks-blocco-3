832d1895f444eff41220a44a7afdeb942c59015c
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 13:20:52 +0200

    push finale

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,23 +1,39 @@
 //creo un array vuoto
 //chiedo all'utente dei numeri fino a quando la loro somma non fa 50
-const container = []
-let somma = 0;
-while(somma < 50 ){
-   let numeriUtente = Number(prompt("inserisci dei numeri"));
-   while(isNaN(numeriUtente)){
-     numeriUtente = Number(prompt("UN NUMERO ARABO"))
-     console.log(container, somma)
-   }
-   if(somma > 50){
-      // console.log("la somma è maggiore di 50");
-      alert("la somma è maggiore di 50")
-   }else{
-      container.push(numeriUtente);
-   somma = somma + numeriUtente;
-   }
-  
-}
-if(somma > 50){
-   // console.log("la somma è maggiore di 50");
-   alert("la somma è maggiore di 50")
-}
+ const container = []
+ let somma = 0;
+ while(somma < 50 ){
+    let numeriUtente = Number(prompt("inserisci dei numeri"));
+    while(isNaN(numeriUtente)){
+      numeriUtente = Number(prompt("UN NUMERO ARABO"))
+      // console.log(container, somma)
+    }
+    somma = somma + numeriUtente;
+    if(somma > 50){
+        console.log("la somma è maggiore di 50");
+       alert("la somma è maggiore di 50")
+    }else if(somma < 50){
+       container.push(numeriUtente);
+    }
+    console.log(container, somma)
+ }
+
+
+
+
+// const container = []
+// let somma = 0;
+// do{
+//    let numeroUtente = Number(prompt("inserisci un numero"));
+//    if(!isNaN(numeroUtente)){
+//       somma = somma + numeroUtente;
+//       if(somma < 50){
+//          container.push(numeroUtente);
+//       }
+      
+//    }
+// }
+// while(somma < 50){
+//    console.log(container);
+// }
+
