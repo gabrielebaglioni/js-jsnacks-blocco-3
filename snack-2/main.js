21c247ecc77ed30b9c9b7502af1f3add0e3bba91
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 15:04:33 +0200

    logica implementata ma non funzioonante

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,3 +4,13 @@ const N = Number(prompt("inserisci un numero"));
 while(isNaN(N)){
    N = Number(prompt("INSERISCI UN NUMERO!!!!"))
 }
+//creo un array vuoto per inserirci N array
+let nArray = [];
+//creo un ciclo che ha i.lengght=N come indice per stampare tanti array quanto è il valore di N
+for(let i = 0; i < N; i++){
+   nArray.push([i]);
+//agiungo nei miei N array 10 numeri casuli da 1 a 100   
+}for(let x = 0; x < 10; x++ ){
+   nArray[i].push(Math.floor(Math.random() * 100) + 1);
+}
+console.log(nArray[i]);
