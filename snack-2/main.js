1721f0cdc9c9614e6f71a23a23184334ec6ac2db
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-05 15:19:45 +0200

    logica funzionante

--- snack-2/main.js
+++ snack-2/main.js
@@ -8,9 +8,12 @@ while(isNaN(N)){
 let nArray = [];
 //creo un ciclo che ha i.lengght=N come indice per stampare tanti array quanto è il valore di N
 for(let i = 0; i < N; i++){
-   nArray.push([i]);
-//agiungo nei miei N array 10 numeri casuli da 1 a 100   
-}for(let x = 0; x < 10; x++ ){
-   nArray[i].push(Math.floor(Math.random() * 100) + 1);
+   // nArray.push([i]);
+   const nArray = []
+//agiungo nei miei N array 10 numeri casuli da 1 a 100 
+      for(let x = 0; x < 10; x++ ){
+         nArray.push(Math.floor(Math.random() * 100) + 1);  
+      }
+      console.log(nArray);
 }
-console.log(nArray[i]);
+
